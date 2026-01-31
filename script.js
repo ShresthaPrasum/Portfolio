a02775a100801720a82b353d53aff7c95c6702bb
prasum shrestha <prasumshrestha8877@gmail.com>
2026-01-31 09:11:22 +0545

    build form system and adding scrolling animation

---
+++ script.js
@@ -0,0 +1,6 @@
+const project_container = document.querySelector(".project-container")
+const projects = project_container.querySelectorAll(".project")
+
+// for (let i = 0; i < projects.length; i++) {
+//     projects[i].style.animationDelay = i * 1.3 + "s";
+// }
